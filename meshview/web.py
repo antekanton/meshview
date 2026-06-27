fa3f662abd5076192900528b41bea27a5f9ecc36
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2026-06-27 23:49:07 +0700

    fix: use correct zip mode instead of suggested by linter

--- meshview/web.py
+++ meshview/web.py
@@ -448,7 +448,7 @@ async def graph_traceroute(request):
     unique_edges = {}
     for path in paths:
         color = '#' + hex(hash(tuple(path)))[3:9]
-        for src, dest in zip(path, path[1:], strict=True):
+        for src, dest in zip(path, path[1:], strict=False):
             if (src, dest) not in unique_edges:
                 unique_edges[(src, dest)] = color
 
